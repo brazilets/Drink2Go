7e3e985f0e4e1aebec475e728971c6ba6251d343
brazilets <brazilets.dev@gmail.com>
2024-10-23 18:04:21 +0300

    Adds styling and adaptivity to the header block

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,4 @@
-/* в этот файл добавляет скрипты*/
+document.getElementById('burger').addEventListener('click', () => {
+  document.querySelector('.menu-nav__list').classList.toggle('active');
+  document.querySelector('.js-toggle-button').classList.toggle('active');
+});
